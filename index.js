8b1461b512e6755332b8276ef3fd44b72c0743d5
angelo <aurtula1@gmail.com>
2021-06-14 19:23:17 -0400

    commit tree 64bddbd3f30d73cbdb827a00bab4da0984a2142e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Ada", "Angelo", "Mark"]
+function writeCards(names) {
+    const thankYous = []
+    for (let i = 0; i < names.length; i++){
+        thankYous.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYous;
+}
+
+function countDown(number){
+    let i = number;
+    while (i > -1) {
+        console.log(i);
+        i--;
+    }
+}
